0e984dbb73875cec4cd65a765a05d1c06458367b
angeredgecko <greg.zbo3@gmail.com>
2019-01-12 11:39:49 -0500

    added based module code

--- src/com/tallpeople/keeptalking/Game.java
+++ src/com/tallpeople/keeptalking/Game.java
@@ -4,6 +4,21 @@ import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.screen.TerminalScreen;
 
 public class Game implements IGame{
+
+    public final int MODULE_WIDTH = 56;
+    public final int MODULE_HEIGHT = 50;
+
+    public final ViewManager viewManager;
+
+    public Game() {
+
+        Module[] frontModules = new Module[3];
+        //frontModules[1] = new Timer(XOffset);
+
+        viewManager = new ViewManager(ViewManager.ViewType.FRONT);
+        viewManager.setModules(ViewManager.ViewType.FRONT, frontModules);
+    }
+
     public void initialize(Engine engine, TerminalScreen screen) {
 
     }
